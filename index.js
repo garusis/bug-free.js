357acc16b28fd31e857422238c84eef0c3933a59
Marcos Alvarez <garusis@gmail.com>
2022-11-21 19:52:13 -0500

    fix: Arreglando integracion entre eslint y prettier

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 const a = "Un texto te ejemplo";
 
-
-function sumar(a, b){
-    return a + b
+function sumar(a, b) {
+  return a + b;
 }
 
-sumar(5)
-sumar(6, 3)
-sumar()
+sumar(5);
+sumar(6, 3);
+sumar();
 
 a = "otra cosa";
 
 // Ejemplo 2.
 
-const c = 5 + 3
-(5 + 3).toString()
+const c = 5 + 3;
 
-
+(5 + 3).toString();
 
 // Ejemplo 3.
 
 function reasignandoArgumentos(numerito) {
-    numerito = 13;
+  numerito = 13;
 }
 
-const argumento = 5
+const argumento = 5;
 reasignandoArgumentos(argumento);
